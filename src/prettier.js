84642db3c931fba9b9b8caef70a3614b8f1d322d
Eli Oshinsky <eli@jcn.io>
2019-06-25 19:08:49 -0400

    add printWidth fis tab size

--- src/prettier.js
+++ src/prettier.js
@@ -1,9 +1,10 @@
 module.exports = {
   trailingComma: 'es5',
-  tabWidth: 4,
+  tabWidth: 2,
   semi: true,
   singleQuote: true,
   useTabs: false,
   bracketSpacing: true,
   arrowParens: 'avoid',
+  printWidth: 80
 };
