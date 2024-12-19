5429cc12dfb6a4da8bf01d1c869e06fb062a896d
Phi Long <long.nguyen@digitalfortress.dev>
2024-12-19 14:09:13 +0700

    chore: fix eslint

--- src/eslint.js
+++ src/eslint.js
@@ -2,25 +2,21 @@ module.exports = {
   extends: [
     "eslint:recommended",
     "plugin:import/recommended",
-    'plugin:prettier/recommended',
-    'prettier',
+    "plugin:prettier/recommended",
+    "prettier",
   ],
   parserOptions: {
-    ecmaVersion: 8
+    ecmaVersion: 8,
+    sourceType: "module",
   },
-  plugins: ["prettier"],
+  plugins: ["prettier", "import"],
   rules: {
     "prettier/prettier": "error",
     "no-console": "off",
     "no-underscore-dangle": "off",
     "class-methods-use-this": "off",
-    "no-unused-vars": [
-      "warn",
-      { "argsIgnorePattern": "^_" }
-    ],
-    "import/no-unresolved": [
-      "error", 
-      { ignore: ['electron'] },
-    ]
+    "no-unused-vars": ["warn", { argsIgnorePattern: "^_" }],
+    "import/no-unresolved": ["error", { ignore: ["electron"] }],
+    "import/no-extraneous-dependencies": "off",
   },
-}
+};
